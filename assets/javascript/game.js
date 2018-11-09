d883faae3bee837bbbd973bd6e0976882c26ef32
mohw <michaelohw5@gmail.com>
2018-11-08 18:44:36 -0800

    intermission

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,7 +16,7 @@ var currentDiv = document.getElementById("current");
 //=====FUNCTIONS=====
 
 //Initialize the game
-var initializeGame = function() {
+var initializeGame = function () {
     wordList = [
         "pink floyd",
         "led zeppelin",
@@ -51,9 +51,9 @@ var pickWord = function (arr) {
 }; //works
 
 //function to check if a letter is in the word
-var checkLetter = function (word, letter) {
-    for (var i = 0; i < word.length; i++) {
-        if (letter === word[i]) {
+var checkLetter = function () {
+    for (var i = 0; i < currentWord.length; i++) {
+        if (currentLetter === currentWord[i]) {
             return true;
         }
     }
@@ -64,7 +64,7 @@ var checkLetter = function (word, letter) {
 //once we check if a letter is in the word, we need to figure out 
 //the indexes... indices? anyway, so we can use it.
 //because of that I think it needs to return an array.
-var indexToReturn = function (word, letter) {
+var indexToChange = function (word, letter) {
     var arrOfIndexes = [];
     for (var i = 0; i < word.length; i++) {
         if (letter === word[i]) {
@@ -75,11 +75,11 @@ var indexToReturn = function (word, letter) {
 }; //works
 
 //replace the word with underline
-var underline = function() {
+var underline = function () {
     arrToReturn = [];
-    for (var i=0; i<currentWord.length; i++) {
-        if(currentWord[i] === " ") {
-            arrToReturn.push("-");
+    for (var i = 0; i < currentWord.length; i++) {
+        if (currentWord[i] === " ") {
+            arrToReturn.push('&nbsp;');
         }
         else {
             arrToReturn.push("_")
@@ -91,29 +91,35 @@ var underline = function() {
 //underline array to string for display
 var underlineDisplay = function () {
     var arr = underline();
-    for (var i=0; i<arr.length; i++) {
-        displayWord += arr[i] + " " ;
+    for (var i = 0; i < arr.length; i++) {
+        displayWord += arr[i] + " ";
     }
     return displayWord;
 }
 
 //String-ify the current word array for ... if i need it
-var currentToString = function() {
+var currentToString = function () {
     var stringToReturn = "";
-    for (var i=0; i<currentWord.length; i++) {
+    for (var i = 0; i < currentWord.length; i++) {
         stringToReturn += currentWord[i];
     }
     return stringToReturn;
 };
 
-var insertToCurrent = function() {
+var insertToCurrent = function () {
     var newP = document.createElement("p");
-    newP.textContent = underline();
+    newP.innerHTML = underline();
     currentDiv.appendChild(newP);
 }
 
+var insertToGuessedLetters = function () {
+    var newP = document.createElement("p");
+    newP.innerHTML = lettersGuessed;
+    guessedDiv.appendChild(newP);
+}
+
 //reset page
-var resetPage = function() {
+var resetPage = function () {
     document.getElementById("correct").textContent = "0";
     document.getElementById("remaining").textContent = remainingGuess;
     document.getElementById("guessedLetters").textContent = lettersGuessed;
@@ -121,36 +127,47 @@ var resetPage = function() {
 };
 
 //reset game
-var resetGame = function() {
+var resetGame = function () {
     pickWord(wordList);
     numWins = 0;
     remainingGuess = 10;
 };
 
+//function to check if input is a letter
+var isLetter = function (ch) {
+    return /^[A-Z]$/i.test(ch);
+};
+
+//gotta change displayWord whenever there is correct guess
+
+
 //check whether the user input is within the currentWord. returns boolean.
 var checkGuess = function () {
-    document.onkeyup = function(e) {
+    document.onkeyup = function (e) {
         var userInput = e.key; //get user input
         currentLetter = e.key;
-        for (var i=0; i<currentWord ; i++) {
-            if(currentWord[i] === userInput) {
-                return true;
+        if (isLetter(userInput)) {
+            for (var i = 0; i < currentWord; i++) {
+                if (currentWord[i] === userInput) {
+                    return true;
+                }
             }
+            lettersGuessed.push(userInput);
+            console.log(lettersGuessed);
         }
-        return false;
+        else {
+           return false;
+        }
     }
 };
 
-
-
-
-
-
-
-
-
-
-
+//if user input is correct then replace underlines with that input
+var displayCorrectLetter = function () {
+    var correctIndexes = indexToChange();
+    for (var i = 0; i<correctIndexes.length; i++) {
+        
+    }
+}
 
 
 // window.onkeydown = function (e) {
@@ -167,13 +184,13 @@ var play = function () {
     initializeGame();
     resetGame();
     resetPage();
+    checkGuess();
     console.log(currentWord.join(""));
     console.log(numWins);
     console.log(remainingGuess);
-    console.log(checkLetter("abcdefg", "f"));
-    console.log(checkLetter("abcdefg", "h"));
     console.log(underline(currentWord));
     console.log(currentToString(currentWord));
+    console.log(lettersGuessed);
 };
 
 //=====PLAY=====
